127e0ffe601a7cfdb16cfb0f1b9b72cbbb056a83
splendid-frame-7429 <bsameera@yahoo.com>
2019-02-27 19:03:19 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    keys = []
+  
+    #Hash.each do |key, value|
+      #arguments.each do |x|
+       # if(value == x)
+      #    keys.push(key)
+       # end 
+      #end 
+    #end 
+    keys 
   end
 end
